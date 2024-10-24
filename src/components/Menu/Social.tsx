9fa57925f49eef7cb6727b536f70340a63f94c72
tudorneagu <neagu.t@gmail.com>
2024-10-24 10:43:02 +0200

    corected typescript bugs

--- src/components/Menu/Social.tsx
+++ src/components/Menu/Social.tsx
@@ -2,11 +2,15 @@ function Social() {
   return (
     <div className="absolute top-[86vh]">
       <div className="flex flex-col gap-2">
-        <a href="https://www.instagram.com/cosmin_ph/">
+        <a className="w-6" href="https://www.instagram.com/cosmin_ph/">
           <img src="/instaLogo.svg" alt="Instagram page link" />
         </a>
         <a href="https://www.instagram.com/cosmin_ph/">
-          <img src="/whatsappLogo.svg" alt="Whatsapp contact link" />
+          <img
+            className="w-6"
+            src="/whatsappLogo.svg"
+            alt="Whatsapp contact link"
+          />
         </a>
       </div>
     </div>
