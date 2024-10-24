23d95e5d66c0ff936c9cf52bdc18e747a4d7dc63
tudorneagu <neagu.t@gmail.com>
2024-10-24 14:10:56 +0200

    UI bug fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -37,7 +37,6 @@ const Home = React.forwardRef<HTMLDivElement>((_, ref) => {
         ref={ref}
         id="home-section"
         className="relative h-screen overflow-clip flex justify-end md:justify-between ">
-        {/* The image positioned on the right side */}
         <div className="min-w-[300px] bg-white z-20 hidden md:block"></div>
         <img
           src={images[currentImageIndex]}
@@ -46,8 +45,6 @@ const Home = React.forwardRef<HTMLDivElement>((_, ref) => {
             fade ? "fade-enter-active" : "fade-exit-active"
           }`}
         />
-
-        {/* The gradient overlay aligned with the image */}
       </div>
     </section>
   );
